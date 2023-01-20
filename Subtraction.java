657f2298aa1198a24df86a1b66180e9036684f07
Himanshu Rajput <chauhanhimanshu982@gmail.com>
2023-01-20 17:10:27 +0530

    Add files via upload

---
+++ Subtraction.java
@@ -0,0 +1,10 @@
+class Subtraction{
+public static void main(String []args){
+System.out.println("The two number is :");
+int a = 20;
+int b = 10;
+System.out.println(a + " " + b);
+int sub = a-b;
+System.out.println("The sub is = " + sub);
+}
+}
