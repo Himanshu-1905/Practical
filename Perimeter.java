f7f3475f5a92ed16c52d438bb075e74a4d7f97a1
Himanshu Rajput <chauhanhimanshu982@gmail.com>
2023-01-20 17:09:06 +0530

    Add files via upload

---
+++ Perimeter.java
@@ -0,0 +1,10 @@
+class Perimeter{
+public static void main(String []args){
+System.out.println("length and breadth is :");
+int l = 5;
+int b = 7;
+System.out.println(l + " " + b);
+int per = 2*(l+b);
+System.out.println("The perimeter of rectangle is = " + per);
+}
+}
