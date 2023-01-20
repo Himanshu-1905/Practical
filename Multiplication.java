862d11f49f35fed64bee196eb176fbf45497ff81
Himanshu Rajput <chauhanhimanshu982@gmail.com>
2023-01-20 17:08:15 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,10 @@
+class Multiplication{
+public static void main(String []args){
+System.out.println("The two number is :");
+int a = 4;
+int b = 5;
+System.out.println(a + " " + b);
+int mul = a*b;
+System.out.println("The mul is = " + mul);
+}
+}
