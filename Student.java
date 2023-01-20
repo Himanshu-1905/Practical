b5711bc3661ab4ef2cb428fa10321bedeef699ae
Himanshu Rajput <chauhanhimanshu982@gmail.com>
2023-01-20 17:09:57 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,25 @@
+class Student1{
+int slno;
+String name;
+
+}
+
+class Student{
+
+public static void main(String...args){
+
+Student1 s = new Student1 ();
+
+s.slno=01;
+s.name="Himanshu";
+
+System.out.println(s.slno);
+System.out.println(s.name);
+
+s.slno=02;
+s.name="Jatin";
+
+System.out.println(s.slno);
+System.out.println(s.name);
+}
+}
