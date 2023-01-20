11aaaf2814e5d0371e2f0aa8d273acb35f5f90db
Himanshu Rajput <chauhanhimanshu982@gmail.com>
2023-01-20 17:06:58 +0530

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,10 @@
+class Area{
+public static void main(String []args){
+System.out.println("length and breadth is :");
+int l = 5;
+int b = 7;
+System.out.println(l + " " + b);
+int area = l*b;
+System.out.println("The area of rectangle is = " + area);
+}
+}
