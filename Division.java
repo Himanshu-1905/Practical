ac5147d758a1f329f596840f9b545a648984cd4c
Himanshu Rajput <chauhanhimanshu982@gmail.com>
2023-01-20 17:07:20 +0530

    Add files via upload

---
+++ Division.java
@@ -0,0 +1,10 @@
+class Division{
+public static void main(String []args){
+System.out.println("The two number is :");
+int a = 50;
+int b = 5;
+System.out.println(a + " " + b);
+int div = a/b;
+System.out.println("The div is = " + div);
+}
+}
