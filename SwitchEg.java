3ead15e734dc62007a37f1f5e255857bbdef8313
Himanshu Rajput <chauhanhimanshu982@gmail.com>
2023-01-30 18:27:34 +0530

    Add files via upload

---
+++ SwitchEg.java
@@ -0,0 +1,28 @@
+
+
+public class SwitchEg {
+	public static void main(String[] args) {
+		int no1=10;//declairing a variable of switch
+		switch(no1) { //Switch expression
+		// case statement(multiple conditions)
+		case 1:
+			System.out.println("20");
+			break;
+		case 2:
+			System.out.println("30");
+			break;
+		case 3:
+			System.out.println("10");
+			break;
+		case 4:
+			System.out.println("50");
+			break;
+		case 5:
+			System.out.println("60");
+			break;
+		default:System.out.println("Invalid number"); //syso
+		
+		}
+	}
+
+}
