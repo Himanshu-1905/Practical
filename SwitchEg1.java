eeafbfa108f93e0a2a9a2dd66c2b11a18b8bea8f
Himanshu Rajput <chauhanhimanshu982@gmail.com>
2023-01-30 18:30:13 +0530

    Add files via upload

---
+++ SwitchEg1.java
@@ -0,0 +1,24 @@
+
+public class SwitchEg1 {
+	public static void main(String[] args) { //main=ctrl+space
+		char ch='h';
+		switch (ch) {
+		case 'a':
+			System.out.println("First case execute");
+			break;
+		case 'b':
+			System.out.println("Second case execute");
+			break;
+		case 'c':
+			System.out.println("Second case execute");
+			break;
+		case 'h':
+			System.out.println("Last case execute");
+			break;
+			default:System.out.println("Invalid");
+			
+		}
+			
+	}
+
+}
