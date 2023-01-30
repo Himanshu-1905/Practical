075b4c76bd17bdf582b47afee0b8fbbe2eba0805
Himanshu Rajput <chauhanhimanshu982@gmail.com>
2023-01-30 18:33:29 +0530

    Add files via upload

---
+++ SimpleForLoop.java
@@ -0,0 +1,12 @@
+
+
+public class SimpleForLoop {
+	public static void main(String[] args) {
+		// code of for loop
+		
+		for(int i=1; i<=10; i++) { //iterate 10 times
+			System.out.println("The output is " + i);
+		}
+	}
+
+}
