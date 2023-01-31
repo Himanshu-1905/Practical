d52aff2ad49019ed5960d42d3faf9d548bbfc455
Himanshu Rajput <chauhanhimanshu982@gmail.com>
2023-01-31 17:22:37 +0530

    Add files via upload

---
+++ WhileLoop.java
@@ -0,0 +1,16 @@
+
+
+public class WhileLoop {
+	public static void main(String[] args) {
+		int i=1;
+		while(i<=10){ // condition at last
+		System.out.println(i);
+		i++;
+		}
+		/*while(true) {
+			System.out.println("Chalte raho");
+			It prints Chalte raho infinite times
+			*/
+		}
+	}
+
