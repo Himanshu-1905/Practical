15d06edc3619815fd7abe71f93370f9314b2c542
Himanshu Rajput <chauhanhimanshu982@gmail.com>
2023-01-31 17:05:32 +0530

    Add files via upload

---
+++ ForEachArr.java
@@ -0,0 +1,16 @@
+
+
+public class ForEachArr {
+	public static void main(String[] args) {
+		int arr[] = {12,24,46,68,80,100}; // declare an array
+		for(int i:arr) { // using for each loop for print the array
+			System.out.println(i);
+		} 
+		/*for(;;) {
+			System.out.println("Chalte raho");
+			It prints chalte raho infinite times
+		*/
+		}
+	}
+
+
