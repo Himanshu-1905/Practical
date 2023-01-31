719e6a43f3fed3df54d081df97fa5a8867349ac6
Himanshu Rajput <chauhanhimanshu982@gmail.com>
2023-01-31 17:44:54 +0530

    Add files via upload

---
+++ DoWhileLoop1.java
@@ -0,0 +1,12 @@
+
+
+public class DoWhileLoop1 {
+	public static void main(String[] args) {
+		int i=0;
+		System.out.println("First 5 even numbers \n");
+		do {
+			System.out.println(i); //statement
+			i=i+2; //increment
+		}while(i<=10); //condition
+		}
+	}
