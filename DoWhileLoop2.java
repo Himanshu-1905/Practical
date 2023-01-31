3ba0b03125f1658e623dc84820cac106bfd8f9bb
Himanshu Rajput <chauhanhimanshu982@gmail.com>
2023-01-31 17:45:08 +0530

    Add files via upload

---
+++ DoWhileLoop2.java
@@ -0,0 +1,12 @@
+
+
+public class DoWhileLoop2 {
+	public static void main(String[] args) {
+		int i=1;
+		System.out.println("First 10 odd numbers \n");
+		do {
+			System.out.println(i); //statement
+			i=i+2; //increment
+		}while(i<=20); //condition
+		}
+	}
