4bf444d604f56e6729e4d4e97a7313bd0de3a1c2
Himanshu Rajput <chauhanhimanshu982@gmail.com>
2023-01-31 17:31:57 +0530

    Add files via upload

---
+++ DoWhileLoop.java
@@ -0,0 +1,18 @@
+
+
+public class DoWhileLoop {
+	public static void main(String[] args) {
+		int i=1;
+		do {
+			System.out.println(i); //statement
+			i++;
+		}while(i<=10); //condition
+		}
+	/*
+	 do {
+		System.out.println("Chalte raho");
+	}while(true); // Infinite do while loop
+	*/
+	}
+
+
