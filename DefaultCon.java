3aeb1e24ed5a753987558bc2dab4bf64e0b15118
Himanshu Rajput <chauhanhimanshu982@gmail.com>
2023-02-08 11:04:52 +0530

    Add files via upload

---
+++ DefaultCon.java
@@ -0,0 +1,10 @@
+
+
+public class DefaultCon {
+	DefaultCon(){ //Default Constructor
+		System.out.println("Default construcor is created");
+	}
+	public static void main(String[] args) {
+		DefaultCon obj = new DefaultCon(); // object creation
+	}
+}
