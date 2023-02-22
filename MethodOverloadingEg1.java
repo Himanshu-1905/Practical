d6b9ee3cc07e2c9b69ae396327042e5b02136ff4
Himanshu Rajput <chauhanhimanshu982@gmail.com>
2023-02-22 20:31:14 +0530

    Add files via upload

---
+++ MethodOverloadingEg1.java
@@ -0,0 +1,9 @@
+
+public class MethodOverloadingEg1 {
+	static int sum (int a, int b) {
+		return a + b;
+	}
+	public static void main(String[] args) {
+		System.out.println(MethodOverloadingEg1.sum(5,6));
+	}
+}
