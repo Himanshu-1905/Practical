b9248d89c725f2ec087da36d0b9dabdbbe897d01
Himanshu Rajput <chauhanhimanshu982@gmail.com>
2023-03-01 21:29:34 +0530

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,25 +1,23 @@
-class Student1{
-int slno;
-String name;
 
-}
-
-class Student{
-
-public static void main(String...args){
-
-Student1 s = new Student1 ();
-
-s.slno=01;
-s.name="Himanshu";
-
-System.out.println(s.slno);
-System.out.println(s.name);
-
-s.slno=02;
-s.name="Jatin";
-
-System.out.println(s.slno);
-System.out.println(s.name);
-}
+public class Student {// class
+	int id;
+	String name;
+	Address address; //aggretion (entity ref of address class)
+	public Student(int id,Address address,String name) {
+		this.id=id;
+		this.name=name;
+		this.address=address;
+	}
+	void display() { //method
+		System.out.println(id+" "+name);
+		System.out.println(address.city+" "+address.state+" "+address.country);
+	}
+	public static void main(String[] args) {
+		Address address1 = new Address("dgp","wb","india");
+		Address address2 = new Address("kol","wb","india");
+		Student s = new Student(12,address1,"Himanshu");
+		Student s1 = new Student(12,address2,"Jatin");
+		s.display();
+		s1.display();
+	}
 }
