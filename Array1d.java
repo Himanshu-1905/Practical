ac7927f3269370c494543dc5c924babbad611d2a
Himanshu Rajput <chauhanhimanshu982@gmail.com>
2023-03-03 12:17:49 +0530

    Add files via upload

---
+++ Array1d.java
@@ -0,0 +1,15 @@
+
+//example of single dimensional array
+public class Array1d { 
+public static void main(String args[]) {
+	String [] names={"Himanshu","Jatin","Harsh","Rahul","Mohit","Rakesh","Gourav","Krish","Amit","Rohit"}; //declaring array
+	for(int i=0;i<names.length;i++) {
+		System.out.println(names[i]);
+	}
+	System.out.println();
+	int[] phone= {123,242,453,564,5465,245,464,234,222,444};
+	for(int j=0;j<phone.length;j++) {
+		System.out.println(phone[j]);
+	}
+}
+}
