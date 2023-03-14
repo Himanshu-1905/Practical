f9ca283ce44ced2c15b4da0f8c7db57c033a1078
Himanshu Rajput <chauhanhimanshu982@gmail.com>
2023-03-14 07:07:17 +0530

    Add files via upload

---
+++ StringLength.java
@@ -0,0 +1,13 @@
+
+// using length() method
+public class StringLength {
+	public static void main(String[] args) {
+		String s = "Anudip Foundation";
+		// one way to find string length
+		System.out.println("The String length of '"+s+"' is : "+s.length());
+		
+		// Get the length of string by another way
+		int len = s.length(); // length by a string
+		System.out.println("The string length of '"+s+"' is: "+len);
+	}
+}
