53974da3cf04d1496cbcdc8eb354d444f8a390ba
Himanshu Rajput <chauhanhimanshu982@gmail.com>
2023-03-14 07:06:50 +0530

    Add files via upload

---
+++ String2.java
@@ -0,0 +1,9 @@
+
+public class String2 {
+	public static void mian(String[] args) {
+		String str ="Himanshu";
+		str.concat("Chauhan"); //insert the string at the end 
+		str=str.concat("Chauhan"); //it will print Himanshu Chauhan
+		System.out.println(str); //it will print Chauhan only because string
+	}
+}
