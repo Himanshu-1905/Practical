481610487795fe57b2a8e9764e3267078f4f0851
Himanshu Rajput <chauhanhimanshu982@gmail.com>
2023-03-22 23:26:46 +0530

    Add files via upload

---
+++ ThrowTest1.java
@@ -0,0 +1,16 @@
+
+//another unchecked example of throw keyword
+public class ThrowTest1 {
+//defining method
+	public static void checknum(int num) {
+		if(num<0) {
+			throw new ArithmeticException("number is negative");
+		}else {
+			System.out.println("number is positive");
+	}
+	}
+public static void main(String[] args) {
+	checknum(-1);
+	System.out.println("exceuted");
+		}
+	}
