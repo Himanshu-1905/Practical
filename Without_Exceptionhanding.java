acb4d4f7d216ddf4babfe2abc135a9a433ccc4fb
Himanshu Rajput <chauhanhimanshu982@gmail.com>
2023-03-22 23:27:44 +0530

    Add files via upload

---
+++ Without_Exceptionhanding.java
@@ -0,0 +1,12 @@
+
+public class Without_Exceptionhanding {
+public static void main(String [] args) {
+	try {
+		int num =10/0;
+	}
+	catch(Exception e) {
+		System.out.println(e);
+	}
+	System.out.println("the end");
+	}
+}
