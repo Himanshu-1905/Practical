113fcc8ab6174e1a32cd81d799aad56f28717bd0
Himanshu Rajput <chauhanhimanshu982@gmail.com>
2023-03-28 18:28:08 +0530

    Add files via upload

---
+++ Runmethod.java
@@ -0,0 +1,13 @@
+
+public class Runmethod extends Thread {
+public void run() {
+	System.out.println("Thread Activated");
+}
+public static void main(String[] args) {
+	Runmethod r1 = new Runmethod();
+	r1.run();
+	r1.setName("Himanshu");
+	String s = r1.getName();
+	System.out.println (s);
+}
+}
